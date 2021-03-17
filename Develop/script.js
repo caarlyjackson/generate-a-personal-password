a6e1344120c8a7d045754b4ac221419696d7a928
Carly Jackson <carlysjackson2@gmail.com>
2021-03-17 13:07:28 +1100

    Commit 2. Large array added.

--- Develop/script.js
+++ Develop/script.js
@@ -2,53 +2,78 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', "M"
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var specialCharacters = ['`', '~', '!', '@', '#', '%', '$', '^', '&', '*', '(', ')', '_', '-', '+', '=', ';', ':', '/', '>', '<', '.', ',']
 var numerical = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var allOptions = []
+
 
 function generatePassword() {
 // Size of password
   var sizeOfPassword = parseInt(prompt("Please enter the password size you want"));
   console.log(sizeOfPassword);
 
-  // Larger than 8, smaller than 128
-  if (sizeOfPassword < 8) {
-    alert("Your password is less than 8 characters")
-    return;
-  }
-  if (sizeOfPassword > 128) {
-    alert("Your password is larger than 128 characters")
-    return;
-  }
+    // Larger than 8, smaller than 128
+if (sizeOfPassword < 8) {
+  alert("Your password is less than 8 characters")
+  return;
+}
+if (sizeOfPassword > 128) {
+  alert("Your password is larger than 128 characters")
+  return;
+}
 
-// Lowercase
+    // Lowercase
   var lowercaseAnswer = confirm("Do you want lowercase letters?");
 
   console.log(lowercaseAnswer);
 
 
-// Uppercase
+    // Uppercase
 var uppercaseAnswer = confirm("Do you want uppercase letters?");
 
 console.log(uppercaseAnswer);
 
 
-// Special characters
-var specialCharactersAnswer = confirm("Do you want special characters?")
+    // Special characters
+var specialCharactersAnswer = confirm("Do you want special characters?");
 
 console.log(specialCharactersAnswer);
 
-// Numerical
-var numericalAnswer = confirm("Do you want to add numbers?")
+
+    // Numerical
+var numericalAnswer = confirm("Do you want to add numbers?");
 
 console.log(numericalAnswer);
 
 for (var i = 0; i < sizeOfPassword; i++) {
   console.log(upperCase[Math.floor(Math.random()*upperCase.length)]);
 }
+
+// for (var i = 0, n = sizeOfPassword.length; i < length; ++i) {
+//   retVal += sizeOfPassword.charAt(Math.floor(Math.random() * n));
+
 }
+
+// Big array Push
+if (specialCharactersAnswer = true) {
+   allOptions.push(specialCharacters);
+}
+
+if (uppercaseAnswer = true) {
+  allOptions.push(upperCase);
+}
+
+if (lowercaseAnswer = true) {
+  allOptions.push(lowerCase);
+}
+
+console.log(allOptions);
+
+
+// const randomElement = allOptions[Math.floor(Math.random() * sizeOfPassword.length)];
 
 
 // Computer generates password
-  // var index = Math.floor(Math.random() * generatePassword.length);
-  // var computerChoice = options[index];
+  var index = Math.floor(Math.random() * generatePassword.length);
+  // var computerChoice = options[allOptions];
 
 
 
