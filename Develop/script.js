47e27e16a73d8fc32067f449b5fc94e34cc2ae79
Carly Jackson <carlysjackson2@gmail.com>
2021-03-18 10:18:34 +1100

    Code nearly complete. Need to fix final password configuration.

--- Develop/script.js
+++ Develop/script.js
@@ -4,91 +4,82 @@ var specialCharacters = ['`', '~', '!', '@', '#', '%', '$', '^', '&', '*', '(', 
 var numerical = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 var allOptions = []
 
-
-function generatePassword() {
-// Size of password
+                          // GET PASSWORD INFO FROM USER
+function getPasswordInfo() {
+// function generatePassword() {
+    // Size of password
   var sizeOfPassword = parseInt(prompt("Please enter the password size you want"));
   console.log(sizeOfPassword);
-
     // Larger than 8, smaller than 128
-if (sizeOfPassword < 8) {
-  alert("Your password is less than 8 characters")
-  return;
-}
-if (sizeOfPassword > 128) {
-  alert("Your password is larger than 128 characters")
-  return;
-}
+          if (sizeOfPassword < 8) {
+            alert("Your password is less than 8 characters")
+          return;
+          }
+          if (sizeOfPassword > 128) {
+            alert("Your password is larger than 128 characters")
+          return;
+          }
 
     // Lowercase
   var lowercaseAnswer = confirm("Do you want lowercase letters?");
-
   console.log(lowercaseAnswer);
 
-
     // Uppercase
-var uppercaseAnswer = confirm("Do you want uppercase letters?");
-
-console.log(uppercaseAnswer);
-
+  var uppercaseAnswer = confirm("Do you want uppercase letters?");
+  console.log(uppercaseAnswer);
 
     // Special characters
-var specialCharactersAnswer = confirm("Do you want special characters?");
-
-console.log(specialCharactersAnswer);
-
+  var specialCharactersAnswer = confirm("Do you want special characters?");
+  console.log(specialCharactersAnswer);
 
     // Numerical
-var numericalAnswer = confirm("Do you want to add numbers?");
+  var numericalAnswer = confirm("Do you want to add numbers?");
+  console.log(numericalAnswer);
 
-console.log(numericalAnswer);
+// Large array - Build (push)
+function generatePassword(){
+  var passwordInfo = getPasswordInfo()
 
-for (var i = 0; i < sizeOfPassword; i++) {
-  console.log(upperCase[Math.floor(Math.random()*upperCase.length)]);
+  if (specialCharactersAnswer = true) {
+      allOptions.push(specialCharacters);
+  }
+  if (uppercaseAnswer = true) {
+      allOptions.push(upperCase);
+  }
+  if (lowercaseAnswer = true) {
+      allOptions.push(lowerCase);
+  }
+
+  var passwordInfo = {
+    length : sizeOfPassword,
+    hasSpecial : specialCharactersAnswer,
+    hasNumeric : numericalAnswer,
+    hasLowercase: lowercaseAnswer,
+    hasUppercase : uppercaseAnswer,
+  }
+  return passwordInfo;
 }
 
-// for (var i = 0, n = sizeOfPassword.length; i < length; ++i) {
-//   retVal += sizeOfPassword.charAt(Math.floor(Math.random() * n));
 
-}
+                          // GENERATE PASSWORD
 
-// Big array Push
-if (specialCharactersAnswer = true) {
-   allOptions.push(specialCharacters);
-}
-
-if (uppercaseAnswer = true) {
-  allOptions.push(upperCase);
-}
-
-if (lowercaseAnswer = true) {
-  allOptions.push(lowerCase);
-}
-
-console.log(allOptions);
-
-
-// const randomElement = allOptions[Math.floor(Math.random() * sizeOfPassword.length)];
-
-
-// Computer generates password
-  var index = Math.floor(Math.random() * generatePassword.length);
-  // var computerChoice = options[allOptions];
-
+// FROM RPS HOMEWORK: Get random index from array of options
+  var index = Math.floor(Math.random() * allOptions.length);
+  var computerChoice = allOptions[index];
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
 
+
+                          // WRITE PASSWORD
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = getPasswordInfo();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
